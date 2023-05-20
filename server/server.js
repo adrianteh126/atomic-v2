f10d88787bdec968a937ed47a469a17a33b06d49
Kuan Kiat <adrianteh02@hotmail.com>
2023-05-20 23:21:19 +0800

    Add Mongoose & Remove Excessive Files

--- server/server.js
+++ server/server.js
@@ -1,25 +1,28 @@
+// Packages
 const express = require("express");
+const path = require("path");
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the "public" folder
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(path.join(__dirname, "public")));
 
+// Routing
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(path.join(__dirname, "index.html"));
 })
 
 app.get("/login.html", function (req, res) {
-    res.sendFile(__dirname + "/login.html");
+    res.sendFile(path.join(__dirname, "login.html"));
 })
 
 app.get("/dashboard.html", function (req, res) {
-    res.sendFile(__dirname + "/dashboard.html");
+    res.sendFile(path.join(__dirname, "dashboard.html"));
 })
 
 app.get("/settings.html", function (req, res) {
-    res.sendFile(__dirname + "/settings.html");
+    res.sendFile(path.join(__dirname, "settings.html"));
 })
 
 // Retrive and calculate from POST method 
@@ -32,15 +35,17 @@ app.get("/settings.html", function (req, res) {
 // })
 
 // 404 Error Handler
-app.use((req, res, lanjiao) => {
+app.use((req, res) => {
     res.status(404).send('<h1>404 Error: Page Not Found</h1>');
 });
 
-// // Retrive data 
+// // Way to retrive data 
 // app.post("/", function (req, res) {
 //     var a = req.body.a; //retrieve a from POST method made
 // })
 
+// App listening on port 3000
+// Can be access by localhost:3000 on browser
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 })
