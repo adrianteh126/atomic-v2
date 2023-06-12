1ff2da2934ad7c82f51fc7c283a46402d84f381b
PeiWei02 <npwlol54@gmail.com>
2023-06-12 21:39:44 +0800

    Statistic page all comppleted'

--- client/src/components/Statistic.vue
+++ client/src/components/Statistic.vue
@@ -1,22 +1,51 @@
 <template>
-    <section>
-      <h4 class="p-4  fw-bold">Statistic📊</h4>
-          <section
-            class="content d-flex justify-content-center align-items-center" 
-            style="height: 60vh;"
+  <div class="container-fluid">
+    <div class="row">
+      <!-- header -->
+
+      <h3 class="py-2 pt-4 px-5 fw-bold">
+        Hey Keat, Lets Check out your productivity!
+      </h3>
+
+      <div class="section">
+        <div class="row">
+          <h5 class="fw-bold px-5" id="taskStatus">
+            This Week Completed: On Going:
+          </h5>
+          <div class="col-md-5" style="height: 80px">
+            <canvas id="stackedBarChart" class="px-5"></canvas>
+          </div>
+        </div>
+      </div>
+
+      <h5 class="fw-bold px-5 pt-5">Overall Statistic</h5>
+      <section class="content d-flex justify-content-center align-items-center">
+        <div class="d-flex">
+          <div
+            class="col-md-4 py-2 px-5 border-0"
+            style="border-right: 1px solid gray"
           >
-            <div class="" style="width: clamp(500px,600px,50vw);">
-              <canvas id="toDoChart"></canvas>
-            </div>
-          </section>
-    </section>    
+            <canvas id="priorityChart"></canvas>
+          </div>
+          <div
+            class="col-md-4 py-2 px-5 border-0"
+            style="border-right: 1px solid gray"
+          >
+            <canvas id="statusChart"></canvas>
+          </div>
+          <div class="col-md-4 py-2 px-5 border-0">
+            <canvas id="weeklyChart"></canvas>
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
 </template>
 
-  <script>
-    export default {
-        name : 'StatisticComponent',
-    }
-  </script>
-  
-  <style>
-  </style>
+<script>
+export default {
+  name: "StatisticComponent",
+};
+</script>
+
+<style></style>
