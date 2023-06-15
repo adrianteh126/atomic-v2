db86f54f5438d4d41a90bc1e7aafcf24673ff0af
waytheking <33257118+waytheking@users.noreply.github.com>
2023-06-15 21:49:30 +0800

    crud dashboard (#5)
    * Add files via upload
    
    * Delete Todos.vue
    
    * Delete DashboardPage.vue
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload

--- client/src/views/DashboardPage.vue
+++ client/src/views/DashboardPage.vue
@@ -13,245 +13,46 @@
       </div>
     </section>
 
-    <!-- Task modal -->
-    <div
-      class="modal fade"
-      id="taskModal"
-      tabindex="-1"
-      aria-labelledby="taskModalLabel"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="taskModalLabel">
-              Modify Task Information
-            </h5>
-          </div>
-          <div class="modal-body">
-            <form>
-              <div class="mb-3">
-                <label for="modalTaskName" class="form-label">Task Name</label>
-                <input type="text" class="form-control" id="modalTaskName" />
-              </div>
-              <div class="mb-3">
-                <label for="modalTaskDesc" class="form-label"
-                  >Task Description</label
-                >
-                <input type="text" class="form-control" id="modalTaskDesc" />
-              </div>
-              <div class="mb-3">
-                <label for="modalTaskProgress">Task Progress (1-10)</label>
-                <input
-                  type="range"
-                  class="form-range"
-                  min="0"
-                  max="100"
-                  step="10"
-                  id="modalTaskProgress"
-                  value="0"
-                />
-              </div>
-              <div class="mb-3">
-                <label for="modalTaskDueDate" class="form-label"
-                  >Task Due Date</label
-                >
-                <input type="text" class="form-control" id="modalTaskDueDate" />
-              </div>
-              <div class="mb-3">
-                <label for="modalTaskPriority">Task Priority</label>
-                <select class="form-select" id="modalTaskPriority">
-                  <option value="Low Priority">Low Priority</option>
-                  <option value="Medium Priority">Medium Priority</option>
-                  <option value="High Priority">High Priority</option>
-                </select>
-              </div>
-            </form>
-          </div>
-          <div class="modal-footer">
-            <button
-              type="button"
-              id="modalCloseBtn"
-              class="btn btn-secondary"
-              data-bs-dismiss="modal"
-            >
-              Close
-            </button>
-            <div id="button-container">
-              <button type="button" id="modalSaveBtn" class="btn btn-success">
-                Save changes
-              </button>
-              <button type="button" id="modalAddBtn" class="btn btn-success">
-                Add Task
-              </button>
-              <button type="button" id="modalDltBtn" class="btn btn-danger">
-                Delete Task
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
 
-    <!-- Reminder modal -->
-    <div class="modal fade" id="reminderModal">
-      <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">
-              Your due date is approaching. Don't forget to complete your task
-              on time!
-            </h5>
-          </div>
-          <!-- card 1 -->
-          <div class="p-3">
-            <div class="card-body border border-1 p-3 rounded-3">
-              <h5 class="card-title fw-bold">Mid Term test for WIA2004</h5>
-              <h5 class="card-subtitle text-secondary">
-                Lecture 1 - Lecture 4
-              </h5>
-              <div class="progress-state my-2">
-                <i class="bx bx-list-ul icon"></i>
-                <span class="text-secondary">Progress</span>
-                <span class="bx-pull-right float-end fw-bold">1/10</span>
-              </div>
-              <div class="progress" style="height: 3px">
-                <div
-                  class="progress-bar bg-danger"
-                  role="progressbar"
-                  style="width: 10%"
-                  aria-valuenow="10"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                ></div>
-              </div>
-              <div class="d-flex gap-1">
-                <div class="">
-                  <p class="card-text mt-1">
-                    <span
-                      class="badge bg-body-secondary circle mr-2"
-                      style="color: #ff7979"
-                      >4 May 2023</span
-                    >
-                  </p>
-                </div>
-                <div class="">
-                  <p class="card-priority mt-1">
-                    <span
-                      class="badge bg-body-secondary circle mr-2"
-                      style="color: #ff7979"
-                      >High Priority</span
-                    >
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- card 2 -->
-          <div class="p-3">
-            <div class="card-body border border-1 p-3 rounded-3">
-              <h5 class="card-title fw-bold">Submission of TCS tutorial</h5>
-              <h5 class="card-subtitle text-secondary">TCS tutorial 3 & 4</h5>
-              <div class="progress-state my-2">
-                <i class="bx bx-list-ul icon"></i>
-                <span class="text-secondary">Progress</span>
-                <span class="bx-pull-right float-end fw-bold">3/10</span>
-              </div>
-              <div class="progress" style="height: 3px">
-                <div
-                  class="progress-bar bg-danger"
-                  role="progressbar"
-                  style="width: 30%"
-                  aria-valuenow="30"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                ></div>
-              </div>
-              <div class="d-flex gap-1">
-                <div class="">
-                  <p class="card-text mt-1">
-                    <span
-                      class="badge bg-body-secondary circle mr-2"
-                      style="color: #ff7979"
-                      >5 May 2023</span
-                    >
-                  </p>
-                </div>
-                <div class="">
-                  <p class="card-priority mt-1">
-                    <span
-                      class="badge bg-body-secondary circle mr-2"
-                      style="color: #ff7979"
-                      >High Priority</span
-                    >
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- card 3 -->
-          <div class="p-3">
-            <div class="card-body border border-1 p-3 rounded-3">
-              <h5 class="card-title fw-bold">Design new UI presentation</h5>
-              <h5 class="card-subtitle text-secondary">Web Programming 1</h5>
-              <div class="progress-state my-2">
-                <i class="bx bx-list-ul icon"></i>
-                <span class="text-secondary">Progress</span>
-                <span class="bx-pull-right float-end fw-bold">2/10</span>
-              </div>
-              <div class="progress" style="height: 3px">
-                <div
-                  class="progress-bar bg-danger"
-                  role="progressbar"
-                  style="width: 20%"
-                  aria-valuenow="20"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                ></div>
-              </div>
-              <div class="d-flex gap-1">
-                <div class="">
-                  <p class="card-text mt-1">
-                    <span
-                      class="badge bg-body-secondary circle mr-2"
-                      style="color: #ff7979"
-                      >5 May 2023</span
-                    >
-                  </p>
-                </div>
-                <div class="">
-                  <p class="card-priority mt-1">
-                    <span
-                      class="badge bg-body-secondary circle mr-2"
-                      style="color: #ff7979"
-                      >High Priority</span
-                    >
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="modal-footer">
-            <button
-              type="button"
-              class="btn btn-default"
-              data-dismiss="modal"
-              id="closeReminderModal"
-            >
-              Close
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+
 
   </template>
 
   <script>
   import SideNavBar from '@/components/SideNavBar.vue';
   import TopBar from '@/components/TopBar.vue';
+  import todocrud from '../modules/todocrud';
+  import { onMounted } from 'vue';
+  import 'bootstrap/dist/js/bootstrap.bundle';
 
-    export default {
+
+
+  export default {
+    
+    
+    setup() {
+
+      const { state, GetAllTodos, newTodo, deleteTodo, editTodo, GetTodoInProgress,GetTodoDone,GetTodoNow} = todocrud()
+
+
+      onMounted(() => {
+        GetAllTodos()
+      })
+
+
+    const getPriorityColor = (priority) => {
+      if (priority === 'Low') {
+        return '#78d700';
+      } else if (priority === 'Medium') {
+        return '#ffa048';
+      } else if (priority === 'High') {
+        return '#ff7979';
+      }
+      return '';
+    };
+
+      return { state, GetAllTodos, newTodo, deleteTodo, editTodo,GetTodoInProgress,GetTodoDone,GetTodoNow,getPriorityColor}
+    },
       components : {
         SideNavBar,
         TopBar,
