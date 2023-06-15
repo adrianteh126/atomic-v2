f1e1b0ba0bfc64bdc718be97139b25f3bc1cbd90
waytheking <33257118+waytheking@users.noreply.github.com>
2023-06-15 22:22:33 +0800

    Add files via upload

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
 
 const app = createApp(App);
 app.use(router);
 
 app.mount('#app')
+app.config.globalProperties.$ = $;
